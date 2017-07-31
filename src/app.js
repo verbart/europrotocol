861bef6d84ef354bae8ccb0ba67db5f24432b536
Artem Verbat <artem.verbat@gmail.com>
2017-07-31 10:38:40 +0300

    Updated google api key

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ angular.module('avarcom', [
   .constant('CONSTANT', {
     API_URL: 'https://avarkom.pw/api/v2',
     API_URL_V2: 'https://avarkom.pw/control',
-    GOOGLE_API_KEY: 'AIzaSyA-Ozt9RQOAbDXS6brHXUlNunpN-A4_Ygs'
+    GOOGLE_API_KEY: 'AIzaSyDzQsbRa6fyUb9SHNuIGrUPMj3_zjAMJtM'
   })
 
   .config(function (
